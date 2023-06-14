0cd277fb24108373f210128d5376b14978622714
speluffo <sergio.peluffo@tradelog.com.ar>
2023-06-13 22:48:50 -0300

    miprimer confirmacion de actualizacion

--- script.js
+++ script.js
@@ -5,9 +5,26 @@ function showAlert(){
 const form =document.getElementById("myForm")
 
 function validateEmail(email) {
-    const regex =
+    const regex =   /^[^\s@]+@[^\s@]+\[^\s@].{2,7}$/
+    return regex.test(email)
+}
+function validateform() {
+    const Inpinputut = document.getElementById("email")
+    const inputValue = input.Value
+
+    if (!validateEmail(inputvalue)) {
+        alert(" por favor")
+    } alse{
+        alert(" por favor")
+    }
 }
 
+form.addEventListener("sumit", function(lala){
+    lala.preventDefault()
+    validateform();
+})
+document.querySelector(GamepadButton.button-menu-toggle)
+addEventListener("")
 
 // document.querySelector("button-menu-toggle")
 //     .addEventListener("click", function() {

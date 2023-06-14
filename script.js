7d2025074748f94c6cf4973635711cb3444d9a36
speluffo <sergio.peluffo@tradelog.com.ar>
2023-06-13 22:03:39 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert("Hola , Esta es una alerta de sde javaScript")
+}
+
+const form =document.getElementById("myForm")
+
+function validateEmail(email) {
+    const regex =
+}
+
+
+// document.querySelector("button-menu-toggle")
+//     .addEventListener("click", function() {
+//            document.querySelector(".nav-links").
+//                       classList.toggle("nav-links-responsive")})
